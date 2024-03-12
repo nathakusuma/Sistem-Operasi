a48ab54a7d78ef1519ea77bb3baad8ab00cabc1d
nathakusuma <nathakusuma07@gmail.com>
2024-03-12 20:26:18 +0700

    feat: soal 1

---
+++ soal1/quoting.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo 'Single Quote ini dengan adanya "Double Quote" di antaranya'
+echo "Double quote dengan \"double quote\" kah?"
+echo "Isi directory 'pwd' berjumlah `ls | wc -l` file"
